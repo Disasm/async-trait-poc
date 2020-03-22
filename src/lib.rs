aa6c0d4d560d5c7a1871fa27906e16c871b81956
Vadim Kaushan <admin@disasm.info>
2020-03-22 14:43:31 +0300

    Separate low-level functions

--- src/lib.rs
+++ src/lib.rs
@@ -12,66 +12,40 @@ pub enum UartError {
     InvalidData
 }
 
-#[derive(Copy, Clone)]
-enum UartState {
-    Idle,
-    Sending(u8, usize),
-}
-
 pub struct Uart {
-    state: UartState
+    busy: bool,
+    error: bool,
+    data: u8,
+    ticks_to_send: usize,
 }
 
 impl Uart {
     pub fn new() -> Self {
         Self {
-            state: UartState::Idle
+            busy: false,
+            error: false,
+            data: 0,
+            ticks_to_send: 0
         }
     }
 
-    fn progress(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), UartError>> {
-        match self.state {
-            UartState::Idle => {
-                Poll::Ready(Ok(()))
-            },
-            UartState::Sending(byte, counter) => {
-                if counter > 0 {
-                    self.state = UartState::Sending(byte, counter - 1);
-                } else {
-                    self.state = UartState::Idle;
-                    if byte == 0xff {
-                        return Poll::Ready(Err(UartError::InvalidData));
-                    }
+    fn write_byte(&mut self, byte: u8) {
+        if !self.busy {
+            self.data = byte;
+            self.busy = true;
+            self.ticks_to_send = 3;
+        }
+    }
+
+    fn make_progress(&mut self) {
+        if self.busy {
+            if self.ticks_to_send == 0 {
+                if self.data == 0xff {
+                    self.error = true;
                 }
-                cx.waker().wake_by_ref();
-                Poll::Pending
-            },
-        }
-    }
-
-    pub fn write_byte(&mut self, cx: &mut Context<'_>, byte: u8) -> Poll<Result<(), UartError>> {
-        match self.state {
-            UartState::Idle => {
-                self.state = UartState::Sending(byte, 5);
-                println!("write_byte({:02x}) - Ok", byte);
-                Poll::Ready(Ok(()))
-            },
-            UartState::Sending(_, _) => {
-                println!("write_byte({:02x}) - WoudlBlock", byte);
-                self.progress(cx)
-            },
-        }
-    }
-
-    pub fn flush(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), UartError>> {
-        match self.state {
-            UartState::Idle => {
-                println!("flush() - Ok");
-                Poll::Ready(Ok(()))
-            },
-            UartState::Sending(_, _) => {
-                println!("flush() - WouldBlock");
-                self.progress(cx)
+                self.busy = false;
+            } else {
+                self.ticks_to_send -= 1;
             }
         }
     }
@@ -85,6 +59,42 @@ impl Serial {
     pub fn new(uart: Uart) -> Serial {
         Self {
             uart
+        }
+    }
+
+    fn progress(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), UartError>> {
+        self.uart.make_progress();
+
+        if self.uart.error {
+            self.uart.error = false;
+            return Poll::Ready(Err(UartError::InvalidData));
+        }
+        if !self.uart.busy {
+            Poll::Ready(Ok(()))
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+
+    pub fn write_byte_nowait(&mut self, cx: &mut Context<'_>, byte: u8) -> Poll<Result<(), UartError>> {
+        if !self.uart.busy {
+            self.uart.write_byte(byte);
+            println!("write_byte({:02x}) - Ok", byte);
+            Poll::Ready(Ok(()))
+        } else {
+            println!("write_byte({:02x}) - WoudlBlock", byte);
+            self.progress(cx)
+        }
+    }
+
+    pub fn flush(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), UartError>> {
+        if !self.uart.busy {
+            println!("flush() - Ok");
+            Poll::Ready(Ok(()))
+        } else {
+            println!("flush() - WouldBlock");
+            self.progress(cx)
         }
     }
 
@@ -125,7 +135,7 @@ impl Future for SerialWriteFuture<'_> {
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         if let Some(byte) = self.data.first() {
-            match self.serial.uart.write_byte(cx, *byte) {
+            match self.serial.write_byte_nowait(cx, *byte) {
                 Poll::Ready(Ok(())) => {
                     self.data = &self.data[1..];
                     cx.waker().wake_by_ref();
@@ -134,7 +144,7 @@ impl Future for SerialWriteFuture<'_> {
                 other => other,
             }
         } else {
-            self.serial.uart.flush(cx)
+            self.serial.flush(cx)
         }
     }
 }
