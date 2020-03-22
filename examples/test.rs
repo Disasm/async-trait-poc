d28d13cd53ad4a1169ded5789326598e3216efb3
Vadim Kaushan <admin@disasm.info>
2020-03-22 15:14:13 +0300

    Add WIP driver

--- examples/test.rs
+++ examples/test.rs
@@ -1,4 +1,22 @@
+#![allow(dead_code)]
+
 use async_trait_poc::*;
+
+struct AsyncDriver<UART> {
+    uart: UART
+}
+
+impl<'a, UART: AsyncWrite<'a>> AsyncDriver<UART> {
+    pub fn new(uart: UART) -> Self {
+        Self {
+            uart
+        }
+    }
+
+    // async fn send_hello(&'a mut self) -> Result<(), UART::Error> {
+    //     self.uart.try_write(b"Hello!").await
+    // }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
